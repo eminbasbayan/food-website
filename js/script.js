8914666cc76970746efb57798cb0a17e1bbf5099
eminbasbayan <emin.basbayan@icloud.com>
2022-05-17 11:59:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const searchForm = document.querySelector(".search-form");
+const cartItem = document.querySelector(".cart-items-container");
+const navbar = document.querySelector(".navbar");
+
+//! buttons
+const searchBtn = document.querySelector("#search-btn");
+const cartBtn = document.querySelector("#cart-btn");
+const menuBtn = document.querySelector("#menu-btn");
+
+searchBtn.addEventListener("click", function () {
+  searchForm.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(searchBtn) &&
+      !e.composedPath().includes(searchForm)
+    ) {
+      searchForm.classList.remove("active");
+    }
+  });
+});
+
+cartBtn.addEventListener("click", function () {
+  cartItem.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(cartBtn) &&
+      !e.composedPath().includes(cartItem)
+    ) {
+      cartItem.classList.remove("active");
+    }
+  });
+});
+
+menuBtn.addEventListener("click", function () {
+  navbar.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+    if (
+      !e.composedPath().includes(menuBtn) &&
+      !e.composedPath().includes(navbar)
+    ) {
+      navbar.classList.remove("active");
+    }
+  });
+});
